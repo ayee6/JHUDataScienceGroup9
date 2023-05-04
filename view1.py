7060777a3e9e9eadbf2ef2b4224c06d3c8b662a8
Steven Bischoff <61480100+stevenbischoff@users.noreply.github.com>
2023-05-04 18:13:36 -0500

    Update view1.py
    add minimum_nights and maximum_nights to most_recent_listings

--- view1.py
+++ view1.py
@@ -26,15 +26,15 @@ cursor.execute(''' CREATE VIEW most_recent_listings AS
                 host_acceptance_rate, host_is_superhost, host_neighbourhood, host_listings_count, \
                 host_total_listings_count, host_verifications, host_has_profile_pic, host_identity_verified, neighbourhood, \
                 neighbourhood_cleansed, latitude, longitude, property_type, room_type, accommodates, \
-                bathrooms, bathrooms_text, bedrooms, beds, amenities, minimum_minimum_nights, maximum_minimum_nights, \
-                minimum_maximum_nights, maximum_maximum_nights, minimum_nights_avg_ntm, maximum_nights_avg_ntm, \
-                has_availability, availability_30, availability_60, availability_90, availability_365, \
-                calendar_last_scraped, number_of_reviews, number_of_reviews_ltm, number_of_reviews_l30d, first_review, \
-                last_review, review_scores_rating, review_scores_accuracy, review_scores_cleanliness, review_scores_checkin, \
-                review_scores_communication, review_scores_location, review_scores_value, license, instant_bookable, \
-                calculated_host_listings_count, calculated_host_listings_count_entire_homes, \
-                calculated_host_listings_count_private_rooms, calculated_host_listings_count_shared_rooms, \
-                reviews_per_month, price
+                bathrooms, bathrooms_text, bedrooms, beds, amenities, minimum_nights, maximum_nights, \
+                minimum_minimum_nights, maximum_minimum_nights, minimum_maximum_nights, maximum_maximum_nights, \
+                minimum_nights_avg_ntm, maximum_nights_avg_ntm, has_availability, availability_30, availability_60, \
+                availability_90, availability_365, calendar_last_scraped, number_of_reviews, number_of_reviews_ltm, \
+                number_of_reviews_l30d, first_review, last_review, review_scores_rating, review_scores_accuracy, \
+                review_scores_cleanliness, review_scores_checkin, review_scores_communication, review_scores_location, \
+                review_scores_value, license, instant_bookable, calculated_host_listings_count, \
+                calculated_host_listings_count_entire_homes, calculated_host_listings_count_private_rooms, \
+                calculated_host_listings_count_shared_rooms, reviews_per_month, price
                 FROM listings
                 GROUP BY id
                 ''')
